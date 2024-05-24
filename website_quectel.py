8cc6a2a437cb530f8940284b2eec202f2ea02244
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2024-05-24 11:51:07 +0530

    Update website_quectel.py

--- website_quectel.py
+++ website_quectel.py
@@ -1,35 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Function to scrape the news items from the website
-def scrape_news():
-    url = 'https://www.quectel.com/company/news-and-pr'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # Find all news items
-        news_items = soup.find_all('div', class_='post--stacked')
-        news_list = []
-        for news_item in news_items[:5]:  # Limit to the first 10 news items
-            # Extract the news title and date
-            news_title = news_item.find('h3', class_='post__title').text.strip()
-            news_date = news_item.find('div', class_='post__meta').text.strip()
-            # Store the news title and date in a dictionary
-            news_list.append({'title': news_title, 'date': news_date})
-        return news_list
-    else:
-        print("Failed to retrieve the webpage. Status code:", response.status_code)
+def scrape_latest_news(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"Failed to load page {url}")
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    articles = soup.find_all('a', class_='group text-black no-underline')
+    
+    news = []
+    for article in articles:
+        title = article.find('span', class_='text-lg text-black mb-3').text.strip()
+        link = article['href']
+        news.append({'title': title, 'link': link})
+    
+    return news
 
 if __name__ == "__main__":
-    # Scrape the news items and store them in an array
-    news_array = scrape_news()
-    if news_array:
-        for idx, news in enumerate(news_array, start=1):
-            print(f"News {idx}:")
-            print("Title:", news['title'])
-            print("Date:", news['date'])
-            print()
-    else:
-        print("No news found.")
+    latest_news = scrape_latest_news("https://www.quectel.com/company/news-and-pr/")
+    for item in latest_news:
+        print(item)
