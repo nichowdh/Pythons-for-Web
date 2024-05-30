fe007da086ff475554cdc5fce426c4957c179fda
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2024-05-30 20:40:11 +0530

    Update website_renesas.py

--- website_renesas.py
+++ website_renesas.py
@@ -1,35 +1,25 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-# Function to scrape the news items from the website
-def scrape_news():
-    url = 'https://www.renesas.com/us/en/about/newsroom'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # Find all news items
-        news_items = soup.find_all('div', class_='post--stacked')
-        news_list = []
-        for news_item in news_items[:5]:  # Limit to the first 10 news items
-            # Extract the news title and date
-            news_title = news_item.find('h3', class_='post__title').text.strip()
-            news_date = news_item.find('div', class_='post__meta').text.strip()
-            # Store the news title and date in a dictionary
-            news_list.append({'title': news_title, 'date': news_date})
-        return news_list
-    else:
-        print("Failed to retrieve the webpage. Status code:", response.status_code)
+# Send a GET request to the URL
+response = requests.get("https://www.renesas.com/us/en/about/newsroom#latest-press-releases")
 
-if __name__ == "__main__":
-    # Scrape the news items and store them in an array
-    news_array = scrape_news()
-    if news_array:
-        for idx, news in enumerate(news_array, start=1):
-            print(f"News {idx}:")
-            print("Title:", news['title'])
-            print("Date:", news['date'])
-            print()
-    else:
-        print("No news found.")
+
+
+# Parse the HTML content with BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find all items with class 'item-wrapper press-releases-item-wrapper'
+items = soup.find_all('div', class_='item-wrapper press-releases-item-wrapper')
+
+# Loop through each item and extract title and date
+for item in items:
+    # Extract title
+    title = item.find('div', class_='item-title press-releases-item-title').text.strip()
+    
+    # Extract date
+    date = item.find('time', class_='datetime press-releases-datetime')['datetime']
+    
+    print(f"Title: {title}")
+    print(f"Date: {date}")
+    print("-" * 20)
