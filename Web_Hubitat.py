836e9544ec733b4abc4bdf79c59163fe6d2b5df3
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2024-07-31 16:03:52 +0530

    Update Web_Hubitat.py

--- Web_Hubitat.py
+++ Web_Hubitat.py
@@ -15,11 +15,11 @@ print("Press Release\n")
 entries = soup.find_all('div', class_='press-releases press__grid')
 
 # Extract title and date for each entry
-for entry in entries:
+for entry in entries[:3]:
     paragraphs = entry.find_all('p')
     for paragraph in paragraphs:
         date = paragraph.find('span').text.strip() if paragraph.find('span') else ''
         title = paragraph.find('a').text.strip() if paragraph.find('a') else ''
         print(f"Title: {title}")
         print(f"Date: {date}")
-        print("---")
+        print()
