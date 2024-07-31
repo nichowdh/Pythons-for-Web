e6a65de841a485997b58a13acfc6f47de7a1235f
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2024-07-31 15:14:53 +0530

    Update Web_Firefly.py

--- Web_Firefly.py
+++ Web_Firefly.py
@@ -12,7 +12,7 @@ soup = BeautifulSoup(response.content, 'html.parser')
 entries = soup.find_all('div', class_='news-list')
 
 # Print all news entries with titles and dates
-for i, entry in enumerate(entries[:5]):  # Limit to first  entries
+for i, entry in enumerate(entries[:3]):  # Limit to first  entries
     title_tag = entry.find('h4').find('a')
     title = title_tag.get_text(strip=True) if title_tag else 'No title'
     date_tag = entry.find('div', class_='other').find('span')
