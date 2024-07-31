6215e5fe5f9e8aa1d68338bc4e0c45ac53d3ff99
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2024-07-31 13:52:50 +0530

    Update Web_ECS.py

--- Web_ECS.py
+++ Web_ECS.py
@@ -12,7 +12,7 @@ soup = BeautifulSoup(response.content, 'html.parser')
 entries = soup.find_all('div', class_='item')
 print("News List: \n")
 # Print the news entries with titles and dates
-for entry in entries:
+for entry in entries[:3]:
     date_tag = entry.find('div', class_='tag-date').find('span', class_='name')
     date = date_tag.get_text(strip=True) if date_tag else 'No date'
     title_tag = entry.find('h4').find('a', class_='title')
