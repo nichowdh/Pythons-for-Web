6614014fee452d2e9875f6570e84e2026d43bf2f
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2024-10-25 21:43:23 +0530

    Update Web_Radxa.py

--- Web_Radxa.py
+++ Web_Radxa.py
@@ -30,7 +30,7 @@ if response.status_code == 200:
 
        
     # Print the titles along with their dates
-    for entry in entries:
+    for entry in entries[:3]:
         print(f"Title: {entry[0]}")
         print(f"Date: {entry[1]}")
         print()
