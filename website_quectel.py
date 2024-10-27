24ecd6494b848c693ea0023bc6d0aaafcb33dce0
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2024-10-27 14:46:35 +0530

    Update website_quectel.py

--- website_quectel.py
+++ website_quectel.py
@@ -1,21 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def scrape_latest_news(url):
     response = requests.get(url)
     if response.status_code != 200:
         raise Exception(f"Failed to load page {url}")
-    
+
     soup = BeautifulSoup(response.text, 'html.parser')
     articles = soup.find_all('a', class_='group text-black no-underline')
-    
+
     news = []
-    for article in articles:
+    for article in articles[:3]:
         title = article.find('span', class_='text-lg text-black mb-3').text.strip()
-        link = article['href']
-        news.append({'title': title, 'link': link})
-    
+        news.append({'title': title})
+
     return news
+
 
 if __name__ == "__main__":
     latest_news = scrape_latest_news("https://www.quectel.com/company/news-and-pr/")
